fd225abc188ec710c9ad495467a2227099f30ce5
RainyXeon <rainyxeon@gmail.com>
2026-03-22 16:20:16 +0700

    add: some slight improvements in navbar brand button

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ copyright_notice_element.innerHTML = `© ${year} PerformanC Website — Licensed
 let isOpen = false
 const mobileQuery = window.matchMedia('(max-width: 1000px)');
 
-const menu_button = document.getElementById('menu-button')
+const nav_brand_button = document.getElementById('nav-brand')
 const menu_close_button = document.getElementById('menu-close-button')
 const menu = document.getElementById('menu')
 
-menu_button.addEventListener('click', () => {
+nav_brand_button.addEventListener('click', () => {
   if (!mobileQuery.matches) return;
   if (!isOpen) {
     menu.classList.add('open')
