d453d23ba9ead1933512899c7c9f02730d246b8d
RainyXeon <rainyxeon@gmail.com>
2026-03-31 21:16:42 +0700

    remove: reload when click on logo or name

--- global/js/renderer.js
+++ global/js/renderer.js
@@ -4,7 +4,7 @@ const header = `
   <div id="menu">
     <menu-modal>
       <menu-modal-header>What do you need?</menu-modal-header>
-      <a class="menu-modal-item mmi-can-close" href="/" >Home</a>
+      <a class="menu-modal-item mmi-can-close" href="#" >Home</a>
       <a class="menu-modal-item mmi-can-close" href="#history">History</a>
       <a class="menu-modal-item mmi-can-close">Projects</a>
       <a class="menu-modal-item mmi-can-close">Member</a>
@@ -14,14 +14,14 @@ const header = `
   </div>
   <nav-constructor>
     <nav id="nav-brand">
-      <a href="/"><nav-item class="icon"></nav-item></a>
-      <a href="/"><nav-item id="nav-brand-name">PerformanC</nav-item></a>
+      <a href="#"><nav-item class="icon"></nav-item></a>
+      <a href="#"><nav-item id="nav-brand-name">PerformanC</nav-item></a>
       <nav-item id="nav-menu-button" logo-checked>
         <menu-icon></menu-icon>
       </nav-item>
     </nav>
     <nav id="nav-button">
-      <a href="/"><nav-item>Home</nav-item></a>
+      <a href="#"><nav-item>Home</nav-item></a>
       <a href="#history"><nav-item>History</nav-item></a>
       <nav-item>Projects</nav-item>
       <nav-item>Member</nav-item>
@@ -31,7 +31,7 @@ const header = `
 `
 
 const footer = `
-  <footer>
+  <footer id="footer">
     <a id="copyright_notice" href="/LICENSE" target="_blank"></a>
     <br>
     <a href="/assets/NOTICE.md" target="_blank">Assets used in <b>/assets/performanc</b> is licensed under the "CC BY-NC-ND 4.0" License </a>
