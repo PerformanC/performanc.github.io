eae48ddd1916abf2f7fe41472fb0d1c68b57f20c
RainyXeon <rainyxeon@gmail.com>
2026-04-01 14:24:15 +0700

    remove: left over file

--- global/js/scroll_to_top_button.js
+++
@@ -1,9 +0,0 @@
-const scroll_to_top_buttons = document.getElementsByClassName('scroll-to-top-button')
-
-for (let i = 0; i < scroll_to_top_buttons.length; i++) {
-  const btn = scroll_to_top_buttons[i];
-  btn.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
-}
-
